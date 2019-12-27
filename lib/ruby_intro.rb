8b72ff9669a8cd27438608ba49c56e43fc850903
David Wu <wudawei01@gmail.com>
2019-12-27 21:13:38 +0000

    done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -29,7 +29,7 @@ end
 # Part 2
 
 def hello(name)
-  "Hello, " + name
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
@@ -43,5 +43,25 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_reader :isbn
+  attr_reader :price
+
+  def initialize(isbn, price)
+    self.isbn = isbn
+    self.price = price
+  end
+
+  def isbn=(value)
+    raise ArgumentError.new('ISBN should not be empty') if value.empty?
+    @isbn = value
+  end
+
+  def price=(value)
+    raise ArgumentError.new('price should be positive') if value <= 0
+    @price = value
+  end
+
+  def price_as_string
+    '$%.2f' % [self.price]
+  end
 end
